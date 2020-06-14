321ef0c1bb20547f23b4dcf67b7d99a721a39410
Alejandra Camacho <camacho.ale94@gmail.com>
2020-06-14 15:32:33 -0500

    Entrada de datos por consola

--- Introduccion/entradas-datos.py
+++ Introduccion/entradas-datos.py
@@ -1,3 +1,3 @@
 # Solicitar datos al usuario desde la consola
-a = input("Proporciona un valor: ")
-print("El valor es: ", a)
+resultado = input("Proporciona un valor: ")
+print("El valor ingresado es: ", resultado)
