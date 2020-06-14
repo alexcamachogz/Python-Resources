4d57c8fbc1a2535930317c5ef159ca09ddc0b17f
Alejandra Camacho <camacho.ale94@gmail.com>
2020-06-14 01:15:13 -0500

    Tipos de datos

---
+++ Introduccion/tipos-datos.py
@@ -0,0 +1,12 @@
+# Literal de tipo entero
+x = 5 
+# Literal de tipo flotante
+y = 10.5
+# Literales de tipos booleano
+z = True
+z = False
+# Literal de tipo string
+a = "Saludos"
+
+# Esta función retorna el tipo de dato de la variable
+type( x )
