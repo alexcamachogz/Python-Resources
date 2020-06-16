91b5853dc8fabebfcb55e82cb04958d385a042fb
Alejandra Camacho <camacho.ale94@gmail.com>
2020-06-15 20:42:04 -0500

    Colecciones: Set

---
+++ Introduccion/set.py
@@ -0,0 +1,29 @@
+# Set es una colección sin orden y sin indices
+
+planetas = {"Marte", "Júpiter", "Venus"}
+print(planetas)
+
+# Longitud del set
+print( len(planetas) )
+
+# Revisar si un elemento está presente
+print("Marte" in planetas)
+
+# Agregar nuevos elementos
+planetas.add("Tierra")
+print(planetas)
+
+# Eliminar elementos
+planetas.remove("Tierra")
+print(planetas)
+re
+# Eliminar con discard, no arroja excepción si no encuentra el elemento a eliminar
+planetas.discard("Júpiter")
+print(planetas)
+
+# Limpiar el set completamente
+planetas.clear()
+print(planetas)
+
+# Eliminar completamente la variable
+del planetas
