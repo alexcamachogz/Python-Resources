458a801ff6051c823711acf2d98719eea0e69de1
Alejandra Camacho <camacho.ale94@gmail.com>
2020-06-21 01:04:43 -0500

    Constantes

---
+++ Introduccion/Temas/Datos/constantes.py
@@ -0,0 +1,6 @@
+# Las constantes se deben definir en su propio módulo (un archivo diferente)
+# Deben de escribirse en mayúsculas y ser separadas con _ si el nombre de esta tiene más de una palabra
+
+MI_CONSTANTE = "Valor de mi constante"
+
+print(MI_CONSTANTE)
